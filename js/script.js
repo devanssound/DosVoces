b1a2411f132876b13c26146867abc2ea9df2b0e1
Devanssound <devanssound@gmail.com>
2023-07-16 20:01:01 -0400

    0.0.1 React.js files - npm install

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example: Changing text color when a button is clicked
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+  document.body.style.color = 'red';
+});
